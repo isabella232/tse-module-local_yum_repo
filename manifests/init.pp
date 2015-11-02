04ae34ece7e625877bdca3a03ce3657ded304f10
Reid Vandewiele <reid@puppetlabs.com>
2015-11-02 13:58:44 -0800

    Work out the initial bugs
    This version passes basic smoke tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,16 +4,25 @@ define local_yum_repo (
   $releasever,
 ) {
   assert_private()
+  require local_yum_repo::setup
+
+  $confdir        = $local_yum_repo::setup::confdir
+  $safe_directory = shellquote($directory)
+
+  $conffile_content = epp("local_yum_repo/yum.conf.epp", {
+    'reposdir'   => $reposdir,
+    'releasever' => $releasever,
+  })
 
   file { $conffile:
     ensure  => file,
     owner   => '0',
     group   => '0',
     mode    => '0644',
-    content => epp_template("local_yum_repo/yum.conf.epp")
+    content => $conffile_content,
   }
 
-  file { $reposdir
+  file { $reposdir:
     ensure => directory,
     owner  => '0',
     group  => '0',
@@ -24,10 +33,19 @@ define local_yum_repo (
   # impossible to set the $releasever yum variable without building and
   # installing a versioned package, so we need to refer to repository
   # definitions that do not use the $releasever variable.
-  exec { "ensure local_yum_repo ${title} reposdir":
+  exec { "ensure ${title} reposdir":
     path    => '/usr/bin:/bin',
     command => "${confdir}/mirror_reposdir ${reposdir} ${releasever} enforce",
     unless  => "${confdir}/mirror_reposdir ${reposdir} ${releasever} noop",
+  }
+
+  # TODO: replace refreshonly=true with an onlyif or unless to compare the
+  # ctime of each rpm file to the repodata xml.
+  exec { "${title} createrepo":
+    path        => '/usr/bin:/bin',
+    cwd         => $title,
+    command     => "createrepo ${safe_directory}",
+    refreshonly => true,
   }
 
 }
--- manifests/package.pp
+++ manifests/package.pp
@@ -1,14 +1,17 @@
 define local_yum_repo::package (
   $directory,
+  $package_name         = $title,
   $releasever           = $::os['release']['major'],
   $basearch             = $::architecture,
   $include_dependencies = true,
+  $timeout              = 600,
 ) {
   require local_yum_repo::setup
 
+  $refname  = regsubst($directory, '[/\\ ]', '_', 'G')
   $confdir  = $local_yum_repo::setup::confdir
-  $conffile = "${confdir}/${name}.conf"
-  $reposdir = "${confdir}/${name}.repos.d"
+  $conffile = "${confdir}/${refname}.conf"
+  $reposdir = "${confdir}/${refname}.repos.d"
 
   # Make sure a repository is created and managed at the location specified by
   # the $directory parameter. Use ensure_resource() to easily ensure that
@@ -22,8 +25,11 @@ define local_yum_repo::package (
 
   exec { "${title} repotrack":
     path    => '/usr/bin:/bin',
-    command => "repotrack -c ${conffile} -a ${basearch} -p ${directory} ${name}",
-    unless  => "[ -f ${directory}/${name}*.rpm ]",
+    command => "repotrack -c ${conffile} -a ${basearch} -p ${directory} ${package_name}",
+    unless  => "/bin/sh -c 'ls \"${directory}/${package_name}-\"*.rpm 2>/dev/null'",
+    timeout => $timeout,
+    require => Exec["ensure ${directory} reposdir"],
+    notify  => Exec["${directory} createrepo"],
   }
 
 }
